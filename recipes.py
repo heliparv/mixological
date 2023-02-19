ba4e561bb29e0cea957bb8dcee4d5cc821c8b428
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2023-02-19 19:44:49 +0200

    Fail to make functional recipe functions

--- recipes.py
+++ recipes.py
@@ -5,19 +5,27 @@ import users
 def add_new_recipe(title, alcohol):
     user_id = users.get_user_id()
     if user_id == 0:
-        return 0
-    command = "INSERT INTO recipes (title, alcohol, user_id) VALUES (:content, :alcohol, user_id)"
+        return -1
+    command = "INSERT INTO recipes (title,alcohol,directions,user_id) VALUES (:content,:alcohol,:directions,:user_id)"
     try:
-        db.session.execute(command, {"title":title, "alcohol":alcohol, "user_id":user_id})
+        directions = ""
+        db.session.execute(command, {"title":title, "alcohol":alcohol, "directions":directions, "user_id":user_id})
         db.session.commit()
         return True
     except:
         return False
 
-#def get_recipe_by_id(id):
-
+def add_directions(recipe_id, text):
+    command = "UPDATE recipes SET directions = text WHERE id = recipe_id"
+    try:
+        db.session.execute(command, {"text":text})
+        db.session.commit()
+        return True
+    except:
+        return False
 
 #TODO
+#def get_recipe_by_id(id):
 #add_directions(recipe_id, text)
 #create_new_ingredient(ingredient)
 #add_ingredient_to_recipe(recipe_id, ingredient_id, quantity)
