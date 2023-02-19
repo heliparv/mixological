57da0a9ceec0fb0cfde22c8525ad3ffdd412d599
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2023-02-19 19:21:37 +0200

    Rough sketch of functions needed for ratings

---
+++ ratings.py
@@ -0,0 +1,9 @@
+from db import db
+from flask import session
+
+#TODO add_rating(recipe_id, user_id, rating)check that user hasn't rated the item already
+#modify_rating(recipe_id, user_id, rating)
+#get_recipe_average_rating(recipe_id)
+#get_user_rating_for_recipe(recipe_id, user_id)
+#rank_recipes_by_average()
+#rank_recipes_by_user_rating()
